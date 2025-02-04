6f0dc930c20ec010a3d22a39da026cabde814b57
Roxann BREMOND <roxannbremond@gmail.com>
2025-02-04 14:47:59 +0100

    Ajout du fichier .js et évolution du volet aside

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+//Ici on va gérer l'apparition de asideFull
+
+let Aside = document.getElementById("aside");
+let MenuPrincipal = document.getElementById("MenuPrincipal")
+
+MenuPrincipal.addEventListener("click", ()=>{
+    aside.classList.toggle("activeTransition");
+})
+
+
