3f6f6956b20f4ee6eba3874a45269f010eac6fe6
Roxann BREMOND <roxannbremond@gmail.com>
2025-02-09 15:42:24 +0100

    Refonte de la strucutre principale de la partie Aside pour un meilleur respect du protocole de codage, ajout des propriétées de développement des menus avec un effet fluide

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,49 @@
 //Ici on va gérer l'apparition de asideFull
 
 let Aside = document.getElementById("aside");
-let MenuPrincipal = document.getElementById("MenuPrincipal")
+let MenuPrincipal = document.getElementById("MenuPrincipal");
 
 MenuPrincipal.addEventListener("click", ()=>{
-    aside.classList.toggle("activeTransition");
+    Aside.classList.toggle("activeTransition");
 })
 
 
+//Ici on va gérer l'apparition des différents menus
+
+let MenuClient = document.getElementById("MenuClients");
+let SousMenuClient = document.getElementById("Clients");
+
+MenuClient.addEventListener("click", ()=>{
+    SousMenuClient.classList.toggle("active");
+    SousMenuClient.classList.toggle("activeTransitionY");
+})
+
+let MenuEmployes = document.getElementById("MenuEmployes");
+let SousMenuEmployes = document.getElementById("Employes");
+
+MenuEmployes.addEventListener("click", ()=>{
+    SousMenuEmployes.classList.toggle("activeTransitionY");
+})
+
+
+let MenuFacturation = document.getElementById("MenuFacturation");
+let Facturation = document.getElementById("Facturation");
+
+MenuFacturation.addEventListener("click", ()=>{
+    Facturation.classList.toggle("activeTransitionY");
+})
+
+
+let MenuEntreprise = document.getElementById("MenuEntreprise");
+let Entreprise = document.getElementById("Entreprise");
+
+MenuEntreprise.addEventListener("click", ()=>{
+    Entreprise.classList.toggle("activeTransitionY");
+})
+
+let MenuParametres = document.getElementById("MenuParametres");
+let Parametres = document.getElementById("Parametres");
+
+MenuParametres.addEventListener("click", ()=>{
+    Parametres.classList.toggle("activeTransitionY");
+})
